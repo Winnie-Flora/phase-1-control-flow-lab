81ff81d0c106bbaa829b172e999a43f842fe203a
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-06-26 03:10:15 +0300

    flow lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let showMessage;
+if (ride <= 400) {
+  showMessage = 'This one is on me!';
+}
+else if (ride > 400 && ride <= 2000){
+  showMessage = 'That will be twenty bucks.';
+}
+else if (ride > 2000 && ride <= 2500){
+  showMessage = 'I will gladly take your thirty bucks.';
+}
+else {
+  showMessage = 'No can do.';
+  }
+  return showMessage;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return "Ok, sounds good.";
+  }
+  else if (city !== 'NYC'){
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+if (tip === 'generous'){
+  message = 'Thank you so much.';
+}
+else if (tip === 'not as generous'){
+  message = "Thank you.";
+}
+else {
+  message = 'Bye.'
+}
+return message;
 }
